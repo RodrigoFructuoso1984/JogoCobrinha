a8196e51c3134ffd1502f86c3456bbbf40f6b4c8
RodrigoFructuoso1984 <fructuoso.digao@gmail.com>
2020-12-07 20:20:22 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -14,9 +14,10 @@ function criarBG() {
 
 function criarCobrinha() {
     for(i=0; i < snake.length; i++ ){
-        context.fillStyle = "write";
-        context.fillRect(snake[1].x);
+        context.fillStyle = "white";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 criarBG();
+criarCobrinha();
